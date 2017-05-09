c9b681deda35d674153eeb1a0d4f70641725cf70
Chris Langford <demilich1988@gmail.com>
2017-05-09 13:40:11 -0400

    Add project files.

---
+++ Extensions/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string allText = File.ReadAllText("C:/Users/kumhoidt/Downloads/macbeth2.txt");
+            Console.WriteLine("Words: " + allText.WordCount());
+            Console.WriteLine("Sentence: " + allText.SentenceCount());
+            Console.WriteLine("Paragraph: " + allText.ParagraphCount());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Extensions/StringExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+
+    /// <summary>
+    /// Extensions for the String type
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Method to count the number of words in a document
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static int WordCount(this string text)
+        {
+            return text.Split(' ').Length;
+        }
+
+        /// <summary>
+        /// Method to count the number of sentences in a document
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static int SentenceCount(this string text)
+        {
+            text = text.Replace("...","");
+            return text.Split('.').Length;
+        }
+
+        public static int ParagraphCount(this string text)
+        {
+            return text.Split('\n').Length;
+        }
+    }
+}
